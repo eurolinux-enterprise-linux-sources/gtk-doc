e361b664ace5b2a5688969a0e6173d8adbe9936c
Alex Baranowski <alex@euro-linux.com>
2020-01-09 21:22:29 +0100

    els-ASAP auto commit file/tag gtk-doc-1.11-5.1.el6.src.rpm

---
+++ src/tests/bugs/src/tester.c
@@ -0,0 +1,124 @@
+/**
+ * SECTION:tester
+ * @short_description: module for gtk-doc unit test
+ *
+ * This file contains non-sense code for the sole purpose of testing the docs.
+ *
+ * As described in http://bugzilla.gnome.org/show_bug.cgi?id=457077 it
+ * returns nothing.
+ */
+
+#include <glib.h>
+
+#include "tester.h"
+
+/**
+ * bug_141869_a:
+ * @pid: arg
+ *
+ * http://bugzilla.gnome.org/show_bug.cgi?id=141869
+ */
+void bug_141869_a (unsigned pid) {
+}
+
+/**
+ * bug_141869_b:
+ * @pid: arg
+ *
+ * http://bugzilla.gnome.org/show_bug.cgi?id=141869
+ */
+void bug_141869_b (signed pid) {
+}
+
+
+/**
+ * bug_379466:
+ * @pid: arg
+ *
+ * http://bugzilla.gnome.org/show_bug.cgi?id=379466
+ */
+void bug_379466 (int
+  pid) {
+}
+
+
+/**
+ * bug_380824:
+ * @arg: arg
+ *
+ * Returns a value.
+ * http://bugzilla.gnome.org/show_bug.cgi?id=380824
+ *
+ * Since: 0.1
+ *
+ * Returns: result
+ */
+int bug_380824 (int arg) {
+  return 0;
+}
+
+
+/**
+ * bug_411739:
+ *
+ * http://bugzilla.gnome.org/show_bug.cgi?id=411739
+ *
+ * Returns: result
+ */
+struct bug *
+bug_411739 (void) {
+  return NULL;
+}
+
+
+/**
+ * bug_419997:
+ * @pid: arg
+ *
+ * http://bugzilla.gnome.org/show_bug.cgi?id=419997
+ */
+void bug_419997 (int const_values) {
+}
+
+
+/**
+ * bug_445693:
+ * @pid: arg
+ *
+ * http://bugzilla.gnome.org/show_bug.cgi?id=445693
+ */
+void bug_445693 (unsigned long pid) {
+}
+
+
+/**
+ * bug_471014:
+ *
+ * http://bugzilla.gnome.org/show_bug.cgi?id=471014
+ *
+ * Returns: result
+ */
+G_CONST_RETURN gchar* G_CONST_RETURN * bug_471014 (void) {
+  return NULL;
+}
+
+
+/**
+ * Bug446648:
+ * @BUG_446648_FOO: foo
+ *
+ * http://bugzilla.gnome.org/show_bug.cgi?id=446648
+ **/
+ 
+ 
+ /**
+ * bug_552602:
+ *
+ * http://bugzilla.gnome.org/show_bug.cgi?id=552602
+ *
+ * Returns: result
+ */
+const char* const * bug_552602 (void) {
+  return NULL;
+}
+
---
+++ src/tests/bugs/src/tester.h
@@ -0,0 +1,146 @@
+#ifndef GTKDOC_TESTER_H
+#define GTKDOC_TESTER_H
+
+#include <glib.h>
+
+/**
+ * bug_324535:
+ * @BUG_324535_A: enum 1
+ * @BUG_324535_B: enum 2
+ * @BUG_324535_C: enum 3
+ *
+ * http://bugzilla.gnome.org/show_bug.cgi?id=324535
+ */
+typedef enum {
+  BUG_324535_A,
+#ifdef GTK_DISABLE_DEPRECATED
+  BUG_324535_B,
+#endif
+  BUG_324535_C,
+} bug_324535;
+
+
+/**
+ * bug_501038:
+ *
+ * http://bugzilla.gnome.org/show_bug.cgi?id=501038
+ */
+struct _bug_501038 {
+  gint a;
+#ifdef GTK_DISABLE_DEPRECATED
+  gint b;
+#else
+  gint _b;
+#endif
+};
+
+
+#define _PADDDING 4
+/**
+ * bug_460127:
+ * @a: field
+ *
+ * http://bugzilla.gnome.org/show_bug.cgi?id=460127
+ */
+struct _bug_460127 {
+  /*< public >*/
+  gint a;
+
+  /*< private >*/
+  union {
+    struct {
+      gint b;
+    } ABI;
+    gpointer _reserved[_PADDDING + 0];
+  } abidata;
+
+};
+
+
+/**
+ * bug_477532:
+ *
+ * http://bugzilla.gnome.org/show_bug.cgi?id=477532
+ */
+GLIB_VAR guint64 (*bug_477532) (void);
+
+
+struct bug {
+  int test;
+};
+
+
+void bug_141869_a (unsigned pid);
+void bug_141869_b (signed pid);
+
+void bug_379466 (int
+  pid);
+
+int bug_380824 (int arg);
+
+struct bug *
+bug_411739 (void);
+
+void bug_419997 (int const_values);
+
+void bug_445693 (unsigned long pid);
+
+G_CONST_RETURN gchar* G_CONST_RETURN *
+bug_471014 (void);
+
+const char* const * bug_552602 (void);
+
+/**
+ * Bug446648:
+ * @BUG_446648_FOO: field
+ *
+ * http://bugzilla.gnome.org/show_bug.cgi?id=446648
+ */
+typedef enum _Bug446648 Bug446648;
+enum _Bug446648 {
+    BUG_446648_FOO
+};
+
+/**
+ * Bug512154:
+ * @index: field
+ *
+ * http://bugzilla.gnome.org/show_bug.cgi?id=512154
+ */
+typedef struct {
+  unsigned long index;
+} Bug512154;
+
+/**
+ * BUG_530758:
+ *
+ * <![CDATA[http://bugzilla.gnome.org/show_bug.cgi?id=530758#c1]]>
+ * 
+ * <ulink url="http://bugzilla.gnome.org/show_bug.cgi?id=530758#c1">Test</ulink>
+ */
+#define BUG_530758 "dummy"
+
+/** 
+ * bug_544172:
+ * @self: object pointer.
+ *
+ * http://bugzilla.gnome.org/show_bug.cgi?id=544172
+ *
+ * Returns: result or %NULL.
+ **/
+typedef char const * (*bug_544172) (char const *self);
+
+/** 
+ * bug_554833:
+ *
+ * http://bugzilla.gnome.org/show_bug.cgi?id=554833
+ **/
+struct _bug_554833 {
+  int i;
+};
+
+struct _bug_554833 *
+                bug_554833_new (void);
+
+#endif // GTKDOC_TESTER_H
+
